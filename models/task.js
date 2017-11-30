1d097da1f0ab2f65e4565ebefbd71c1cf55746cf
Márcio Oliveira <marciomjmo@gmail.com>
2017-11-30 01:55:19 -0200

    Implemented the rescheduling and request methods of tasks

--- models/task.js
+++ models/task.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose'),
 
 var moment = require('moment');
 
+var request = require('request');
+
 var taskSchema = new Schema({
   callback_url: {
     type: String,
@@ -76,24 +78,106 @@ var taskSchema = new Schema({
 });
 
 taskSchema.methods.run = function() {
-  console.log("Calling URL: " + this.callback_url);
+  // Calling the callback_url
+  var request = require('request');
+
+  var requestParams = {
+    method: this.http_method,
+    uri: this.callback_url,
+  };
+
+  if ( this.data.length > 0 )
+  {
+    requestParams.multipart = [ { 'content-type': 'application/json', body: this.data } ];
+  }
+
+  request( requestParams,
+    function (error, response, body) {
+      if (response === undefined )
+      {
+        console.log( "[error] request failed without a response" );
+      }
+      else if(response.statusCode == 201){
+        console.log("[ " + this.callback_url + " RESULT]");
+        console.log(body);
+      } else {
+        console.log('error: '+ response.statusCode)
+        console.log(body)
+      }
+    }
+  ).auth( this.auth_user, this.auth_pass, false, this.auth_token );
+
+  // Updating the next call date
   this.updateNextCallDate();
 };
 
 taskSchema.methods.updateNextCallDate = function() {
-  // Set the next date to 1 minute later
+
+  // UPDATING THE REPETITION
   var next_date = moment(this.next_call_date);
-  next_date.add(1, 'm');
+  if ( this.repeat_type == 'EVERY_SECOND' )
+    next_date.add( this.repeat_every, 's' );
+  else if ( this.repeat_type == 'DAILY' )
+    next_date.add( this.repeat_every, 'd' );
+  else if ( this.repeat_type == 'WEEKLY' )
+  {
+    var weekDay = next_date.weekday()%7;
+    var lastWeekDay = this.repeat_weekly_mask.lastIndexOf('1');
+    if ( weekDay == lastWeekDay )
+    {
+      var firstWeekDay = this.repeat_weekly_mask.indexOf('1');
+      // if we are in the last week day call, we go subtract the difference
+      // to the first week day call and add a week to the next date
+      // (equivalent to call the first week day in the next week).
+      var deltaDays = lastWeekDay - firstWeekDay;
+      // repeat weekly must have a special treatment (check week mask)
+      next_date.subtract( deltaDays, 'd' ).add( this.repeat_every, 'w' );
+    }
+    else
+    {
+      // If it is not the last week day call, look for the next day
+      // and add it to the date.
+      for( var i = weekDay + 1; i < this.repeat_weekly_mask.length; i++ )
+      {
+        if ( this.repeat_weekly_mask[i] == '1' )
+        {
+          var delta = i - weekDay;
+          next_date.add( delta, 'd' );
+          break;
+        }
+      }
+    }
+  }
+  else if ( this.repeat_type == 'MONTHLY' )
+    next_date.add( this.repeat_every, 'M' );
+  else if ( this.repeat_type == 'YEARLY' )
+    next_date.add( this.repeat_every, 'y' );
+  else
+    this.ended = true;
 
   this.next_call_date = next_date.format();
 
+  // CHECKING THE END OF THE TASK SCHEDULING
+  if ( this.end_type == 'END_BY_COUNTER' )
+  {
+    this.end_count--;
+    if (this.end_count == 0 )
+      this.ended = true;
+  }
+  else if ( this.end_type == 'END_BY_DATE' )
+  {
+    if ( moment(this.next_call_date) > moment(this.end_date) )
+      this.ended = true;
+  }
+
+  // SAVE CHANGES
   this.save(function(err, obj) {
     if (err) {
       console.log("Error to update task: " + obj.id);
     } else {
       console.log("Next call after update: " + obj.next_call_date);
     }
-  })
+  });
 
 };
 
